226bb9202d09cd9385197f874f19031255accf70
Micaela Asselborn <asselborn96@gmail.com>
2025-12-10 10:18:28 -0300

    Se crea DB y se añaden tablas
    - Se creo base de datos "patitas_felices"
    - Se añadio tabla "duenos"
    - Se añadio tabla "mascotas"
    - Se añadio tabla "veterinarios"
    - Se añadio tabla "historial_clinico"

---
+++ patitas_felices.sql
@@ -0,0 +1,47 @@
+-- Ejercicio 1 – Crear Base de Datos
+CREATE DATABASE patitas_felices;
+
+-- Ejercicio 2 – Crear tabla duenos
+CREATE TABLE
+    duenos (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        nombre VARCHAR(50) NOT NULL,
+        apellido VARCHAR(50) NOT NULL,
+        telefono VARCHAR(20),
+        direccion VARCHAR(100)
+    );
+
+-- Ejercicio 3 – Crear tabla mascotas
+CREATE TABLE
+    mascotas (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        nombre VARCHAR(50) NOT NULL,
+        especie VARCHAR(30) NOT NULL,
+        fecha_nacimiento DATE,
+        id_dueno INT,
+        FOREIGN KEY (id_dueno) REFERENCES duenos (id)
+    );
+
+-- Ejercicio 4 – Crear tabla veterinarios
+CREATE TABLE
+    veterinarios (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        nombre VARCHAR(50) NOT NULL,
+        apellido VARCHAR(50) NOT NULL,
+        matricula VARCHAR(20) NOT NULL UNIQUE,
+        especialidad VARCHAR(50) NOT NULL
+    );
+
+-- Ejercicio 5 – Crear tabla historial_clinico
+CREATE TABLE
+    historial_clinico (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        id_mascota INT,
+        FOREIGN KEY (id_mascota) REFERENCES mascotas (id),
+        id_veterinario INT,
+        FOREIGN KEY (id_veterinario) REFERENCES veterinarios (id),
+        fecha_nacimiento DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+        descripcion VARCHAR(250) NOT NULL
+    );
+
+-- Ejercicio 6 – Insertar registros
